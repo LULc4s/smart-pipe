8f750a4637ba1f55a081d891a40c24f58fb6df1a
Lucas S. Tavares <lucasstavares.computerengineer@gmail.com>
2025-11-30 20:30:16 -0300

    Update escutar.py

--- test/escutar.py
+++ test/escutar.py
@@ -1,13 +1,43 @@
+import csv
 import paho.mqtt.client as mqtt
+from datetime import datetime
+
+BROKER = "localhost"     # ou IP do seu broker
+PORT = 1883
+TOPIC = "dados/sensor"
+
+CSV_FILE = "dados_recebidos.csv"
+
+try:
+    with open(CSV_FILE, "r"):
+        pass
+except FileNotFoundError:
+    with open(CSV_FILE, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["timestamp", "volume_real", "volume_hidrometro", "volume_total"])
+
+def on_connect(client, userdata, flags, rc):
+    print("Conectado ao broker com código:", rc)
+    client.subscribe(TOPIC)
 
 def on_message(client, userdata, msg):
-    print(f"Mensagem recebida: {msg.payload.decode()}")
+    payload = msg.payload.decode()
+ 
+    partes = payload.split(",")
+
+    timestamp = datetime.now().isoformat()
+
+    linha = [timestamp, partes[0], partes[1], partes[2]]
+
+    with open(CSV_FILE, "a", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(linha)
+
+    print("Salvo no CSV:", linha)
 
 client = mqtt.Client()
-client.connect("localhost", 1883, 60)
-
-client.subscribe("teste/topico")
+client.on_connect = on_connect
 client.on_message = on_message
 
-print("Escutando tópico...")
+client.connect(BROKER, PORT, 60)
 client.loop_forever()
